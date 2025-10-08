67b4e06d6e2144373a0b133fa083d00d5d0c4aed
ewatemi20 <lasisitemidayosarah@gmail.com>
2025-10-08 23:02:33 +0100

    commit

--- TSQ_TODO_APP.py
+++ TSQ_TODO_APP.py
@@ -7,7 +7,11 @@ root.title("TSQ TODO APP")
 root.geometry("400x400")
 root.configure(bg="#399adb")
 
-#entry path
+#ENTRY PATH
+entry=tk.Entry(root,width=25,font=("Arial",14))
+entry.grid(row=0,column=0,padx=10,pady=10)
 
+button=tk.Button(root,text="Add Task",width=10,bg="#2ecc71",fg="white",font=("Arial",10))
+button.grid(row=0,column=1)
 
 root.mainloop()
