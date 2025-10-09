85aca3342abe0ee6d4aceb219e82a05480e6cf32
ewatemi20 <lasisitemidayosarah@gmail.com>
2025-10-09 15:19:33 +0100

    commit

--- TSQ_TODO_APP.py
+++ TSQ_TODO_APP.py
@@ -8,7 +8,7 @@ root.geometry("400x400")
 root.configure(bg="#399adb")
 
 #ENTRY PATH
-entry=tk.Entry(root,width=25,font=("Arial",14),justify="right")
+entry=tk.Entry(root,width=25,font=("Arial",14))
 entry.grid(row=0,column=0,padx=10,pady=10)
 
 button=tk.Button(root,text="Add Task",width=10,bg="#2ecc71",fg="#ecf0f1",font=("Arial",10))
