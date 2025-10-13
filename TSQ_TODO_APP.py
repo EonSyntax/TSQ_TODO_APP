07b07064cc9878a8b8805bc74c7a2a0d0d9c5650
ewatemi20 <lasisitemidayosarah@gmail.com>
2025-10-13 14:56:13 +0100

    commit

--- TSQ_TODO_APP.py
+++ TSQ_TODO_APP.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+import tkinter.messagebox as mbox
 
 # MAIN WINDOW FRAME
 root = tk.Tk()
@@ -11,9 +11,31 @@ root.configure(bg="#399adb")
 entry=tk.Entry(root,width=25,font=("Arial",14))
 entry.grid(row=0,column=0,padx=10,pady=10)
 
-button=tk.Button(root,text="Add Task",width=10,bg="#2ecc71",fg="#ecf0f1",font=("Arial",10))
-button.grid(row=0,column=1,padx=6)
+#FUNCTION FOR ENTRY BUTTON
+def  add_task():
+    task=entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        last_index =listbox.size() - 1
+        listbox.itemconfig(last_index,"",fg="blue")
+        entry.delete(0, tk.END)
+        save_task()
+        mbox.showinfo("Info", f"You entered: {task}")
 
+    elif task == "":
+        mbox.showwarning("Warning", "Please enter something — field is empty")
+        
+    else:
+        pass
+
+    
+def save_task():
+        tasks = listbox.get(0, tk.END)
+        print("Saving:", tasks)
+
+
+button=tk.Button(root,text="Add Task",width=10,bg="#2ecc71",fg="#ecf0f1",font=("Arial",10),command=add_task)
+button.grid(row=0,column=1,padx=6)
 
 #LISTBOX PATH
 listbox = tk.Listbox(root, width=40, height=12, font=("Arial", 12))
